7ca834ba58fe8467f534eb2b10271fb6e615b9ff
charles peters <charlespeters42@gmail.com>
2015-03-28 00:16:52 -0400

    updating index.js to includePaths and resolve stuff

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-module.exports = require('./_resetatron.scss');
+var path = require('path');
+var resetatronPath = require.resolve('./_resetatron.scss');
+
+var resetatronDir = path.dirname(resetatronPath);
+
+function includePaths() {
+  return [resetatronDir];
+}
+
+module.exports = {
+
+  includePaths: includePaths(),
+
+  with: function() {
+    var paths  = Array.prototype.slice.call(arguments);
+    var result = [].concat.apply(includePaths(), paths);
+    return result;
+  }
+
+};
